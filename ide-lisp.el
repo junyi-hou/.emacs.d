02e68eec9b68c77bf1a9c351e55632665b1746be
Junyi <junyi.hou@berkeley.edu>
2019-04-02 09:48:00 -0700

    lisp: change hook

--- ide-lisp.el
+++ ide-lisp.el
@@ -1,13 +1,18 @@
 ;;; ide-lisp.el -- setup ide features (auto-completion, code-jump, linter) for lisp
 
+;;; Commentary:
+
+;;; Code:
+
 (require 'dev-autocomplete)
 (require 'dev-linter)
 (require 'dev-jump)
 
 ;; load stuffs
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
-(add-hook 'emacs-lisp-mode-hook 'dumb-jump-mode)
-(add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
+(add-hook 'emacs-lisp-mode-hook (lambda ()
+                                  (company-mode 1)
+                                  (dumb-jump-mode 1)
+                                  (flycheck-mode 1)))
 
 (provide 'ide-lisp)
 ;;; ide-lisp.el ends here
