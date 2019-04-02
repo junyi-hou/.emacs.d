43a8400da6d358637316579a22639b21011805eb
Junyi <junyi.hou@berkeley.edu>
2019-04-02 09:33:27 -0700

    jump: change the way we define keymaps

--- dev-jump.el
+++ dev-jump.el
@@ -4,7 +4,15 @@
   :commands (dumb-jump-mode dumb-jump-go dumb-jump-quick-look dumb-jump-go-other-window dumb-jump-back)
   :init
   (setq dumb-jump-selector 'ivy
-        dumb-jump-force-searcher 'ag))
+        dumb-jump-force-searcher 'ag)
+  :general
+  (:keymaps '(motion normal visual)
+   :prefix "SPC"
+   
+   "jg" 'dumb-jump-go-other-window
+   "jG" 'dumb-jump-go
+   "jb" 'dumb-jump-back
+   "jj" 'dumb-jump-quick-look))
 
 (provide 'dev-jump)
 ;;; dev-jump.el ends here
