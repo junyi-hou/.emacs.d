f7b1626069c81ccc6919ecfcb22442eebac5d3d3
Junyi <junyi.hou@berkeley.edu>
2019-04-25 13:42:40 -0700

    finish lisp-ide

--- bc-ide-lisp.el
+++ bc-ide-lisp.el
@@ -1,14 +1,15 @@
-;;; ide-lisp.el -- setup ide features (auto-completion, code-jump, linter) for lisp -*- lexical-binding: t; -*-
+;;; bc-ide-lisp.el -- setup ide features (auto-completion, code-jump, linter) for lisp -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
 ;;; Code:
 
-(require 'dev-autocomplete)
-(require 'dev-linter)
-(require 'dev-jump)
+(require 'bc-autocomplete)
+(require 'bc-linter)
+(require 'bc-jump)
 
 ;; load stuffs
+(require 'hl-defined)
 
 (eval-after-load 'dash '(dash-enable-font-lock)) ; syntax highlight for dash
 
@@ -17,5 +18,7 @@
                                   (dumb-jump-mode 1)
                                   (flycheck-mode 1)))
 
-(provide 'ide-lisp)
-;;; ide-lisp.el ends here
+
+
+(provide 'bc-ide-lisp)
+;;; bc-ide-lisp.el ends here
