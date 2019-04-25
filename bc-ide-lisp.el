a38c42d6b0b2394f0abfbf507d5362a02cc386c9
Junyi <junyi.hou@berkeley.edu>
2019-04-25 14:06:33 -0700

    add highlight for lisp functions

--- bc-ide-lisp.el
+++ bc-ide-lisp.el
@@ -11,12 +11,18 @@
 ;; load stuffs
 (require 'hl-defined)
 
+(custom-set-faces
+ '(hdefd-functions ((t (:inherit font-lock-function-name-face))))
+ '(hdefd-variables ((t (:inherit font-lock-constant-face))))
+ )
+
 (eval-after-load 'dash '(dash-enable-font-lock)) ; syntax highlight for dash
 
 (add-hook 'emacs-lisp-mode-hook (lambda ()
                                   (company-mode 1)
                                   (dumb-jump-mode 1)
-                                  (flycheck-mode 1)))
+                                  (flycheck-mode 1)
+                                  (hdefd-highlight-mode 1)))
 
 
 (provide 'bc-ide-lisp)
