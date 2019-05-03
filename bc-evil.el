6f57b511cca957619bad29359c369f1212578b84
Junyi <junyi.hou@berkeley.edu>
2019-05-02 22:31:45 -0700

    evil - add `rename-current-file-and-buffer'

--- bc-evil.el
+++ bc-evil.el
@@ -59,6 +59,22 @@
   (if (not (bc-evil--is-user-buffer))
       (bc-evil-previous-user-buffer)))
 
+;; borrow from http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun bc-evil-rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
 (evil-define-motion bc-evil-next-three-lines ()
   (interactive)
   (evil-next-visual-line 3)
@@ -150,6 +166,9 @@
  "gl" 'magit-log-buffer-file
 
  ;; search and replace
+
+ ;; change files
+ "mr" 'bc-evil-rename-file-and-buffer
 
  ;; other uses
  "t" 'evilnc-comment-or-uncomment-lines
