d909b75b38909756de4b908cbaab111f56ff665c
Junyi <junyi.hou@berkeley.edu>
2019-05-08 21:02:19 -0700

    use dumb-jump in elisp mode

--- bc-ide-lisp.el
+++ bc-ide-lisp.el
@@ -7,20 +7,24 @@
 (require 'bc-lsp)
 
 ;; elisp does not have a lsp
-;; only thing, for now, that I miss is jumping to other places
+;; only thing, for now, whatt I miss is jumping to definitions
 (use-package dumb-jump
+  :defer t
+  :config
+  (setq dumb-jump-selector 'ivy
+        dumb-jump-prefer-searcher 'ag)
   :general
   (:keymaps 'emacs-lisp-mode-map
    :states '(normal visual motion)
    :prefix "SPC"
-   "jd" 'dumb-jump-jump))
+   "jd" 'dumb-jump-go))
 
 (eval-after-load 'dash '(dash-enable-font-lock)) ; syntax highlight for dash
 
 (add-hook 'emacs-lisp-mode-hook (lambda ()
                                   (setq tab-width 2)
                                   (company-mode 1)
-                                  (flymake-mode 1)))
+                                  (dumb-jump-mode 1)))
 
 (provide 'bc-ide-lisp)
 ;;; bc-ide-lisp.el ends here
