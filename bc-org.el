fbf37e9638e655348da05b38bc076061a152e80e
Junyi <junyi.hou@berkeley.edu>
2019-06-01 16:44:56 -0700

    update org-mode setting

--- bc-org.el
+++ bc-org.el
@@ -10,21 +10,28 @@
   :config
 
   ;; general config
-  (setq org-default-notes-file (concat org-directory "/notes.org")
-        org-agenda-files '("~/org/")
-        org-agenda-skip-scheduled-if-done t
-        org-todo-keywords
+  (setq org-todo-keywords
         '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE")
-          (sequence "|" "CANCELED" "SOMEDAY"))
+          (sequence "|" "CANCELED" "SOMEDAY")))
+  (add-hook 'org-mode-hook #'company-mode)
+  (add-hook 'org-mode-hook (lambda () (setq tab-width 2)))
 
-        ;; src block settings
-        org-src-fontify-natively t
+  ;; src block settings
+  (setq org-src-fontify-natively t
         org-src-tab-acts-natively t
         org-confirm-babel-evaluate nil)
+  (unless bc-venv-current-venv
+    (bc-venv--enable-venv)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((emacs-lisp . t)
+       (python . t)
+       (jupyter . t))))
+  (advice-add 'org-edit-src-code :after (lambda () (ignore-errors (eglot-ensure))))
+
+  ;; org-capture
 
   (add-hook 'org-capture-mode-hook #'evil-insert-state)
-  (add-hook 'org-mode-hook #'company-mode)
-
   (setq org-capture-templates
         `(("t"
            "A todo item with deadline"
@@ -36,18 +43,12 @@
           )
         )
 
-  ;; babel blocks
-  (unless bc-venv-current-venv
-    (bc-venv--enable-venv)
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((emacs-lisp . t)
-       (python . t)
-       (jupyter . t))))
-
-  (advice-add 'org-edit-src-code :after (lambda () (ignore-errors (eglot-ensure))))
-
   :general
+  (:keymaps 'org-mode-map
+   :states '(normal visual motion)
+   :prefix "SPC"
+   "r" 'org-export-dispatch)
+
   (:keymaps 'org-mode-map
    :states 'normal
    ;; do not need `evil-jump-item'
@@ -57,8 +58,7 @@
    :states '(normal visual motion)
    "<up>" 'org-previous-visible-heading
    "<down>" 'org-next-visible-heading
-   "U" 'outline-up-heading
-   "r" 'org-refile)
+   "U" 'outline-up-heading)
 
   (:keymaps 'org-mode-map
    :states '(insert normal visual motion)
@@ -67,13 +67,7 @@
    "M-j" 'org-shiftdown
    "M-k" 'org-shiftup
    "M-h" 'org-shiftleft
-   "M-l" 'org-shiftright)
-
-  (:keymaps '(normal motion visual)
-   :prefix "SPC"
-   "yc" 'counsel-org-capture
-   "ya" 'org-agenda))
-
+   "M-l" 'org-shiftright))
 
 ;; functions
 
