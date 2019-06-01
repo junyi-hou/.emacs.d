f70ea66aea9a4d5a20e7d2538853327a758f800d
Junyi <junyi.hou@berkeley.edu>
2019-06-01 11:25:14 -0700

    lsp - use posframe-control

--- bc-lsp.el
+++ bc-lsp.el
@@ -18,7 +18,7 @@
 
   :config
   ;; load my doc-viewer
-  (use-package eglot-posframe
+  (use-package posframe-control
     :after eglot
     :load-path "~/Documents/projects/posframe-collection"
     :init
@@ -42,6 +42,37 @@
   "Switch to previously open buffer."
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(defun eglot-posframe-doc-show ()
+  "Update `eglot--help-buffer' with helps of `symbol-at-point' and display it in a `posframe' frame."
+  (interactive)
+  (eglot--dbind ((Hover) contents range)
+      (jsonrpc-request (eglot--current-server-or-lose) :textDocument/hover
+                       (eglot--TextDocumentPositionParams))
+    (when (seq-empty-p contents) (eglot--error "No hover info here"))
+    (when (posframe-workable-p)
+      (let* ((blurb (eglot--hover-info contents range))
+             (sym (thing-at-point 'symbol))
+             (total-height (min 40 (/ (length blurb) 80))))
+
+        (posframe-control-show
+         (eglot--help-buffer)
+         :poshandler (lambda (_info) '(-1 . 0))
+         :string blurb
+         :internal-border-width 3
+         :internal-border-color "gray80"
+         :left-fringe 1
+         :right-fringe 1
+         :width 80
+         :min-width 80
+         :height total-height)))))
+
+(defun eglot-posframe-def-show ()
+  "Show the context of definition in a `posframe' frame."
+  (interactive)
+  ()
+  )
+
 
 (provide 'bc-lsp)
 ;;; bc-lsp.el ends here
