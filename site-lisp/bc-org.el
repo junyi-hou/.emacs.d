c0128a9f3fad520adb44b8378f9feb3670cf2c76
Junyi <junyi.hou@berkeley.edu>
2019-07-13 00:50:16 -0700

    change org mode settings, add some snippets

--- site-lisp/bc-org.el
+++ site-lisp/bc-org.el
@@ -10,6 +10,15 @@
 
   ;; functions
 
+  ;; taken from https://github.com/dakra/dmacs/blob/master/init.org#org
+  (defun bc-org-remove-all-results ()
+    "Remove results from every code block in buffer."
+    (interactive)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward org-babel-src-block-regexp nil t)
+        (org-babel-remove-result))))
+
   (defun bc-org--complete-keywords ()
     "Allow company to complete org keywords after ^#+"
     (add-hook
@@ -30,54 +39,67 @@
                     (shell-command-to-string "echo -n $(date +%H:%M)")
                     ">")))
 
+  (defun bc-org--enable-jupyter-interpreter ()
+    "Add jupyter family to `org-babel-load-languages'."
+    (unless bc-venv-current-venv
+      (bc-venv--enable-venv)
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '((emacs-lisp . t)
+         (latex . t)
+         (shell . t)
+         (python . t)
+         (jupyter . t)))))
+
   :config
 
+  ;; enable latex aucotomplete
   (use-package company-auctex
     :init
-    (defun bc-org-company-add-latex-backends ()
-      (setq-local company-backends
-                  '((company-auctex-labels
-                     company-auctex-bibs
-                     company-auctex-environments
-                     company-auctex-macros
-                     company-auctex-symbols
-                     company-capf
-                     company-yasnippet
-                     company-files)
-                    (company-dabbrev
-                     company-abbrev))))
+    (defun bc-org--company-add-latex-backends ()
+      (let* ((backends company-backends)
+             (car (car backends))
+             (cdr (cdr backends))
+             (bes '(company-auctex-labels
+                          company-auctex-bibs
+                          company-auctex-environments
+                          company-auctex-macros
+                          company-auctex-symbols)))
+        (setq-local company-backends (append (list `(,@bes ,@car)) cdr))))
+
     :hook
-    (org-mode . bc-org-company-add-latex-backends))
+    (org-mode . bc-org--company-add-latex-backends))
 
   ;; general config
   (setq org-cycle-emulate-tab nil
-        org-export-with-toc nil)
+        org-export-with-toc nil
+        org-highlight-latex-and-related '(latex entities script))
 
   (add-hook 'org-mode-hook #'company-mode)
-  (add-hook 'org-mode-hook (lambda () (setq tab-width 2)))
   (add-hook 'org-mode-hook #'org-indent-mode)
+  (add-hook 'org-mode-hook (lambda () (setq tab-width 2)))
   (add-hook 'org-mode-hook #'bc-org--complete-keywords)
+
+  ;; babel
+  ;; load interpreters
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (latex . t)
+     (shell . t)))
 
   ;; src block settings
   (setq org-src-fontify-natively t
         org-src-tab-acts-natively t
         org-confirm-babel-evaluate nil)
 
-  (unless bc-venv-current-venv
-    (bc-venv--enable-venv)
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '((emacs-lisp . t)
-       (latex . t)
-       (shell . t)
-       (python . t)
-       (jupyter . t))))
+  ;; display/update images in the buffer after I evaluate
+  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
 
-  ;; (advice-add 'org-edit-src-code :after (lambda ()
-  ;;                                         (ignore-errors (eglot-ensure))))
-
-  ;; export to latex
+  ;; export to latex and other formats
   (setq org-latex-packages-alist '(("" "setspace")
+                                   ;; https://github.com/gpoore/minted/issues/92
+                                   ("cache=false" "minted")
                                    ("" "pdflscape")
                                    ("" "multirow")
                                    ("" "multicol")
@@ -85,7 +107,6 @@
                                    ("" "amsthm")
                                    ("" "amssymb")
                                    ("" "listingsutf8")
-                                   ("" "minted")
                                    ("top=1in, bottom=1in, left=1in, right=1in" "geometry")
                                    ("" "natbib")))
   (setq
@@ -94,24 +115,9 @@
    '("pdflatex -shell-escape -output-directory %o %f"
      "biber %b"
      "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-     "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
-   )
-  
-  ;; org-capture
+     "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
-  ;; (add-hook 'org-capture-mode-hook #'evil-insert-state)
-  ;; (setq org-capture-templates
-  ;;       `(("t"
-  ;;          "A todo item with deadline"
-  ;;          entry
-  ;;          (file "todo.org"),
-  ;;          (concat "* TODO %?\n"
-  ;;                  "CREATED:  %t\n")
-  ;;          :empty-lines 1)
-  ;;         )
-  ;;       )
-
-  :general
+:general
   (:keymaps 'org-mode-map
    :states '(normal visual motion)
    :prefix "SPC"
