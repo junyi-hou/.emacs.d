8d6e87dad311653da4817f759ac42a0f933a5504
Junyi <junyi.hou@berkeley.edu>
2019-08-18 15:09:25 -0700

    add eshell settings

--- site-lisp/bc-eshell.el
+++ site-lisp/bc-eshell.el
@@ -6,25 +6,65 @@
 
 (require 'bc-core)
 (require 'bc-company)
-
 (use-package eshell
   :init
   (setq eshell-scroll-to-bottom-on-input 'all
+        eshell-buffer-maximum-lines 12000
         eshell-error-if-no-glob t
+        eshell-glob-case-insensitive t
         eshell-hist-ignoredups t
+        eshell-history-size 5000
         eshell-save-history-on-exit t
         eshell-prefer-lisp-functions nil
+        eshell-list-files-after-cd t
         eshell-destroy-buffer-when-process-dies t)
 
   (unless (file-exists-p (no-littering-expand-var-file-name "eshell/alias"))
+    (require 'em-alias)
     ;; if alias file does not exists, define aliases
     (eshell/alias "su" "eshell/su $*")
     (eshell/alias "sudo" "eshell/sudo $*")
-    (eshell/alias "ff" "find-file $*")
+    (eshell/alias "ff" "find-file $1")
     (eshell/alias "cls" "bc-eshell-clear-buffer")
     (eshell/alias "ll" "ls -Aloh --color=always"))
 
   ;; functions
+  ;; https://www.emacswiki.org/emacs/EshellFunctions
+  (defun eshell/unpack (file &rest args)
+    "Unpack FILE with ARGS using default command."
+    (let ((command (some (lambda (x)
+                           (if (string-match-p (car x) file)
+                               (cadr x)))
+                         '((".*\.tar.bz2" "tar xjf")
+                           (".*\.tar.gz" "tar xzf")
+                           (".*\.bz2" "bunzip2")
+                           (".*\.rar" "unrar x")
+                           (".*\.gz" "gunzip")
+                           (".*\.tar" "tar xf")
+                           (".*\.tbz2" "tar xjf")
+                           (".*\.tgz" "tar xzf")
+                           (".*\.zip" "unzip")
+                           (".*\.Z" "uncompress")
+                           (".*" "echo 'Could not unpack the file:'")))))
+      (let ((unpack-command (concat command " " file " " (mapconcat 'identity args " "))))
+        (eshell/printnl "Unpack command: " unpack-command)
+        (eshell-command-result unpack-command))))
+
+  (defun bc-eshell-toggle-sudo ()
+    "Add/Remove sudo in the begining of command line."
+    (interactive)
+    (save-excursion
+      (let ((commands (buffer-substring-no-properties
+                       (eshell-bol) (point-max))))
+        (if (string-match-p "^sudo " commands)
+            (progn
+              (eshell-bol)
+              (while (re-search-forward "sudo " nil t)
+                (replace-match "" t nil)))
+          (progn
+            (eshell-bol)
+            (insert "sudo ")
+            )))))
 
   (defun bc-eshell-open-here ()
     "Open a new shell in the pwd of the current buffer.  If there is already a eshell buffer open for that directory, switch to that buffer."
@@ -64,12 +104,14 @@
   (defun bc-eshell--keymaps ()
     "Define keymap for eshell."
     (general-define-key
-     :states 'normal
+     :states '(normal visual motion)
      :keymaps 'eshell-mode-map
-     "A" 'bc-eshell-goto-prompt)
+     "A" 'bc-eshell-goto-prompt
+     "H" 'eshell-bol
+     "S" 'bc-eshell-toggle-sudo)
 
     (general-define-key
-     :states 'normal
+     :states '(normal visual motion)
      :keymaps 'eshell-mode-map
      :prefix "SPC"
      "q" 'kill-buffer-and-window))
