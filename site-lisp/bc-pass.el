561a0145c9c28937d64ca5a6f6401fd9d1295ec2
Junyi <junyi.hou@berkeley.edu>
2019-08-20 06:01:23 -0700

    better comment

--- site-lisp/bc-pass.el
+++ site-lisp/bc-pass.el
@@ -7,15 +7,14 @@
 (use-package password-store
   :defer t
   :init
-  ;; for CLI pinentry
+  ;; use emacs minibuffer as pinentry frontend
   (defun pinentry-emacs (desc prompt ok error)
     "Use emacs as frontend of pinentry for gpg"
     (let ((str (read-passwd (concat (replace-regexp-in-string "%22" "\"" (replace-regexp-in-string "%0A" "\n" desc)) prompt ": "))))
       str))
 
-  (start-process-shell-command "gpg-agent" nil "gpg-agent --deamon")
+  (start-process-shell-command "gpg-agent" nil "gpg-agent --deamon"))
 
-  )
 
 (provide 'bc-pass)
 ;;; bc-pass.el ends here
