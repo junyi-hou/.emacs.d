8707722a3b1dae4a70c9ee2b3fb9f94c5f306666
Junyi <junyi.hou@berkeley.edu>
2019-08-26 22:38:31 -0700

    auto apply ansi-color to .log files

--- site-lisp/bc-ide-gentoo.el
+++ site-lisp/bc-ide-gentoo.el
@@ -9,6 +9,11 @@
 (add-to-list 'auto-mode-alist '("package\\.accept_keywords" . sh-mode))
 (add-to-list 'auto-mode-alist '("package\\.mask" . sh-mode))
 (add-to-list 'auto-mode-alist '("package\\.use" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.log\\'" . bc-ide-gentoo--apply-ansi-color))
+
+(defun bc-ide-gentoo--apply-ansi-color ()
+  "Color the whole buffer using ansi code."
+  (ansi-color-apply-on-region (point-min) (point-max)))
 
 ;; TODO: generate functions/yasnippets to build ebuild
 
