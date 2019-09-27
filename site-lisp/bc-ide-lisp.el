c684127c3111c9ca98b491bcdad8c4a48c2baa44
Junyi <junyi.hou@berkeley.edu>
2019-09-27 15:27:19 -0700

    add posframe integration to built-in help mode

--- site-lisp/bc-ide-lisp.el
+++ site-lisp/bc-ide-lisp.el
@@ -4,35 +4,65 @@
 
 ;;; Code:
 
-;; for code jump
-;; just need posframe-control.el
-;; (require 'eglot-posframe)
+(use-package posframe-control
+  :quelpa
+  (posframe-control
+   :repo "junyi-hou/posframe-control"
+   :fetcher github))
 
-;; (defconst lisp-posframe-reference-buffer "*lisp-posframe-reference*")
-;; (defconst lisp-posframe-control-buffer "*lisp-posframe-reference-xref*")
+(defconst bc-ide-lisp-help-keymap
+  (let ((map (make-sparse-keymap)))
+    (suppress-keymap map t)
+    (define-key map (kbd "J") (lambda () (interactive)
+                                (posframe-control-next-three-lines
+                                 (help-buffer))))
+    (define-key map (kbd "K") (lambda () (interactive)
+                                (posframe-control-prev-three-lines
+                                 (help-buffer))))
+    (define-key map (kbd "C-d") (lambda () (interactive)
+                                  (posframe-control-scroll-down
+                                   (help-buffer))))
+    (define-key map (kbd "C-u") (lambda () (interactive)
+                                  (posframe-control-scroll-up
+                                   (help-buffer))))
+    (define-key map (kbd "RET") #'bc-ide-lisp-goto-def)
+    map)
+  "Keymap for controlling help posframes.")
 
-;; ;; create a posframe, populate the buffer using `describe-symbol'
-;; ;; control: back/forward/enter the file
-;; (defun lisp-posframe-show-help ()
-;;   "docstring"
-;;   (interactive)
-;;   (posframe-show
-;;    lisp-posframe-reference-buffer
-;;    :poshandler 'posframe-poshandler-point-bottom-left-corner
-;;    :internal-border-width 3
-;;    :internal-border-color "gray80"
-;;    :left-fringe 1
-;;    :right-fringe 1
-;;    :width 85
-;;    :min-width 85
-;;    :height 35
-;;    ))
+(defun bc-ide-lisp-goto-def ()
+  "Goto definition found by the help buffer."
+  (interactive)
+  (with-current-buffer (help-buffer)
+    (button-activate (button-at (next-char-property-change 1))))
+  (posframe-control-hide (help-buffer)))
 
-(use-package elisp-mode
-  :ensure nil
-  :hook
-  (emacs-lisp-mode . (lambda () (setq-local tab-width 2)))
-  (lisp-interaction-mode . (lambda () (setq-local tab-width 2))))
+(defun bc-ide-lisp--posframe-frontend (&rest _)
+  "Use `posframe' to show *Help* buffer."
+  ;; step 1: hide help window
+  (delete-window (get-buffer-window (help-buffer)))
+  ;; step 2: show posframe tips
+  (posframe-control-show
+   (help-buffer)
+   :poshandler 'posframe-poshandler-point-bottom-left-corner
+   :internal-border-width 3
+   :internal-border-color "gray80"
+   :left-fringe 1
+   :right-fringe 1
+   :width 60
+   :min-width 60
+   :height 15
+   :control-keymap bc-ide-lisp-help-keymap
+   :hide-fn (lambda () (interactive)
+              (posframe-control-hide (help-buffer)))))
+
+(advice-add 'describe-function :after #'bc-ide-lisp--posframe-frontend)
+(advice-add 'describe-variable :after #'bc-ide-lisp--posframe-frontend)
+
+(defun bc-ide-lisp--set-tab-width ()
+  (setq-local tab-width 2))
+
+(add-hook 'emacs-lisp-mode-hook #'bc-ide-lisp--set-tab-width)
+(add-hook 'lisp-interaction-mode-hook #'bc-ide-lisp--set-tab-width)
 
 (provide 'bc-ide-lisp)
 ;;; bc-ide-lisp.el ends here
