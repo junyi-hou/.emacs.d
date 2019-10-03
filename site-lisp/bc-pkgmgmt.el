1158f8642e67b870696a13520c10d5f519666f96
Junyi <junyi.hou@berkeley.edu>
2019-10-03 00:04:03 -0700

    turns out need defvar

--- site-lisp/bc-pkgmgmt.el
+++ site-lisp/bc-pkgmgmt.el
@@ -6,6 +6,7 @@
 ;;; Code:
 
 ;; bootstrapping straight.el
+(defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
