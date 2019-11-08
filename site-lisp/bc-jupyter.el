4a6d6673cabeaa800e5e29e986772a5c9fe473dd
Junyi <junyi.hou@berkeley.edu>
2019-11-07 22:07:22 -0800

    remove home-made script for jupyter to start remote kernel

--- site-lisp/bc-jupyter.el
+++ site-lisp/bc-jupyter.el
@@ -16,77 +16,17 @@
   (jupyter-tramp-file-name-p "~/.bashrc")
 
   ;; functions
-  (defun bc-jupyter--start-repl (kernel &optional remote)
-    "Initiate a REPL for KERNEL and attach it to the current buffer.
-If REMOTE is provided, start an remote kernel and connect to it.  Furthermore, if REMOTE is non-nil, prompt the user to set directory."
-      (if remote
-          (let* ((directory
-                  (ivy-read
-                   "directory: "
-                   #'read-file-name-internal
-                   :initial-input
-                   (apply
-                    #'format
-                    (cons "/ssh:%s@%s#%s:/home/junyi/Documents/Research" remote))))
-                 (connection-file
-                  (bc-jupyter--start-remote-kernel kernel remote directory)))
-            (jupyter-connect-repl connection-file
-                                  kernel
-                                  (current-buffer)))
-        (jupyter-run-repl kernel kernel (current-buffer))))
 
-  (defun bc-jupyter--uniquify-process-name ()
-    "Uniquify name of the jupyter kernel process name.  Return the process name"
-    (let* ((base-name "jupyter-remote-kernel")
-           (name-regexp (format "[[:blank:]]\*%s-?\\([[:digit:]]+\\)?\*" base-name))
-           (existing-remote-kernel-number
-            (car (seq-sort
-                  #'>
-                  (mapcar (lambda (buffer)
-                            (let ((buffer-name (buffer-name buffer)))
-                              (if (string-match name-regexp buffer-name)
-                                  (if (match-string 1 buffer-name)
-                                      (string-to-number (match-string 1 buffer-name))
-                                    0)
-                                -1)))
-                          (buffer-list))))))
-      (format "%s%s"
-              base-name
-              (if (> existing-remote-kernel-number -1)
-                  (format "-%d" (1+ existing-remote-kernel-number))
-                ""))))
-
-  (defun bc-jupyter--get-remote-connection-file (remote jupyter-remote-kernel-buffer)
-    "Return the connection file from JUPYTER-REMOTE-KERNEL-BUFFER located in REMOTE."
-    (with-current-buffer jupyter-remote-kernel-buffer
-      (goto-char 1)
-      (when (re-search-forward ":[[:blank:]]\\([/_\.0-9a-zA-Z-]+\.json\\)")
-        (apply #'format (cons "/ssh:%s@%s#%s:%s" (append remote (match-string 1)))))))
-
-  (defun bc-jupyter--start-remote-kernel (kernel remote &optional directory)
-    "Start a remote KERNEL at REMOTE.  If DIRECTORY is non-nil, cd to the DIRECTORY on the REMOTE.  Return the tramp location of the connection file.
-REMOTE is a list of the user name, the server address, and the port number."
-    (let* ((remote-dir (append remote (if directory `(,directory) `(""))))
-           (default-directory
-             (apply #'format (cons "/ssh:%s@%s#%s:%s" remote-dir)))
-           (remote-process-name (bc-jupyter--uniquify-process-name))
-           (remote-process-buffer (format " *%s*" remote-process-name)))
-      ;; start kernel
-      (start-file-process
-       remote-process-name
-       remote-process-buffer
-       (format "/home/%s/.local/bin/jupyter-console" (car remote))
-       "--kernel"
-       kernel)
-      (bc-jupyter--get-remote-connection-file remote remote-process-buffer)))
-
-  (defun bc-jupyter-start-or-switch-to-repl (kernel &optional remote)
-    "Switch to REPL associated the current buffer.  If there is no REPL associated with the current buffer, start one according to KERNEL type.  If REMOTE is non-nil, open a remote kernel by calling `bc-jupyter--start-remote-kernel'."
+  ;; there is good enough built-in support for remote-kernel
+  ;; (i.e., when the file edited is a remote file, start repl in the remote directory)
+  ;; so I can delete the home-made patch of starting remote kernel.
+  (defun bc-jupyter-start-or-switch-to-repl (kernel)
+    "Switch to REPL associated the current buffer.  If there is no REPL associated with the current buffer, start one according to KERNEL type."
     (interactive)
     (if jupyter-current-client
         (jupyter-repl-pop-to-buffer)
       (let ((code-buffer (current-buffer)))
-        (bc-jupyter--start-repl kernel remote)
+        (jupyter-run-repl kernel kernel (current-buffer))
         (jupyter-repl-pop-to-buffer)
         (switch-to-buffer-other-window code-buffer))))
 
