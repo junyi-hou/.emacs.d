bee007e008aca65fce6f0214c22c54ad42d44609
Junyi <junyi.hou@berkeley.edu>
2019-12-09 18:53:15 -0800

    Revert "use ado-mode instead"
    This reverts commit 9cb96e8e34ca799ae6e490f02761f67cbc53be1b.

--- site-lisp/bc-ide-stata.el
+++ site-lisp/bc-ide-stata.el
@@ -4,13 +4,10 @@
 
 ;;; Code:
 
-(use-package ado-mode
-  :straight (ado-mode
-             :repo "louabill/ado-mode"
-             :host github)
-  :init
-  (define-derived-mode jupyter-stata-mode ado-mode "Jupyter-Stata"
-    "This allows `org-mode' to correctly highlights `jupyter-stata' src blocks without using `ess-mode'. To do so, please install `jupyter-emacs' package to enable `ob-jupyter', and the stata kernel (pip install stata-kernel --user) to enable jupyter to talk to stata kernel."))
+(use-package stata-mode
+  :straight (stata-mode
+             :repo "junyi-hou/stata-mode"
+             :host github))
 
 (provide 'bc-ide-stata)
 ;;; bc-ide-stata.el ends here
