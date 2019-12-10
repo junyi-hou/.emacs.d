602780dd9e6fa08b2066625934936b77b1f61568
Junyi <junyi.hou@berkeley.edu>
2019-12-09 20:16:18 -0800

    add jupyter support

--- site-lisp/bc-ide-stata.el
+++ site-lisp/bc-ide-stata.el
@@ -7,7 +7,31 @@
 (use-package stata-mode
   :straight (stata-mode
              :repo "junyi-hou/stata-mode"
-             :host github))
+             :host github)
+  :config
+  (require 'bc-jupyter)
+
+  (defalias 'bc-stata-local-repl
+    (lambda () (interactive) (bc-jupyter-start-or-switch-to-repl "stata"))
+    "Open a jupyter repl for stata interpreter.")
+
+  (defalias 'bc-stata-reconnect
+    #'jupyter-repl-restart-kernel
+    "Reconnect to the current REPL.")
+
+  :general
+  (:states '(motion normal visual)
+   :keymaps 'python-mode-map
+   :prefix "SPC"
+   "rb" 'jupyter-eval-buffer
+   "rf" 'jupyter-eval-defun
+   "rr" 'jupyter-eval-line-or-region
+   "rc" 'bc-python-eval-class
+
+   "ro" 'bc-stata-local-repl
+
+   "rz" 'jupyter-repl-associate-buffer
+   "rZ" 'bc-stata-reconnect))
 
 (provide 'bc-ide-stata)
 ;;; bc-ide-stata.el ends here
