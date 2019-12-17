d45bd4fc8655caf76f8750bcd4e9b9325a7c8108
Junyi <junyi.hou@berkeley.edu>
2019-12-16 18:45:34 -0800

    remove xref-childframe for now

--- site-lisp/bc-lsp.el
+++ site-lisp/bc-lsp.el
@@ -22,23 +22,23 @@
         eglot-put-doc-in-help-buffer t
         eglot-stay-out-of '(company)))
 
-(use-package eglot-childframe
-  :hook
-  (eglot--managed-mode . eglot-childframe-mode)
-  (emacs-lisp-mode . eglot-childframe-mode)
-  (lisp-interaction-mode . eglot-childframe-mode)
+;; (use-package eglot-childframe
+;;   :hook
+;;   (eglot--managed-mode . eglot-childframe-mode)
+;;   (emacs-lisp-mode . eglot-childframe-mode)
+;;   (lisp-interaction-mode . eglot-childframe-mode)
 
-  :straight (eglot-childframe
-             :host github
-             :repo "junyi-hou/eglot-childframe"
-             :branch "elisp-backend-support")
-  :general
-  (:keymaps 'eglot-childframe-mode-map
-   :states '(normal visual motion)
-   :prefix "SPC"
-   "rh" 'eglot-childframe-help
-   "rd" 'eglot-childframe-definition
-   "rj" 'eglot-childframe-reference))
+;;   :straight (eglot-childframe
+;;              :host github
+;;              :repo "junyi-hou/eglot-childframe"
+;;              :branch "elisp-backend-support")
+;;   :general
+;;   (:keymaps 'eglot-childframe-mode-map
+;;    :states '(normal visual motion)
+;;    :prefix "SPC"
+;;    "rh" 'eglot-childframe-help
+;;    "rd" 'eglot-childframe-definition
+;;    "rj" 'eglot-childframe-reference))
 
 (provide 'bc-lsp)
 ;;; bc-lsp.el ends here
