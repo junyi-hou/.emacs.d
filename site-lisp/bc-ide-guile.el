dee74137ea340d1ba76d2cb420bc38735f78d398
Junyi <junyi.hou@berkeley.edu>
2019-12-20 23:27:25 -0800

    scheme REPL: switch from jupyter to geiser

--- site-lisp/bc-ide-guile.el
+++ site-lisp/bc-ide-guile.el
@@ -9,77 +9,55 @@
   :hook
   (scheme-mode . bc-guile--init)
   :init
-  (require 'bc-jupyter)
-
   ;; use guile
   (setq scheme-program-name "guile"
-        geiser-default-implementation 'guile)
+        geiser-default-implementation 'guile
+        ;; unmap everything in geiser-mode-map
+        geiser-repl-mode-map (make-sparse-keymap))
 
   (setq-local tab-width 2)
 
-  (defalias 'bc-guile-local-repl
-    (lambda () (interactive)
-      (bc-jupyter-start-or-switch-to-repl "guile"))
-    "Open a jupyter repl for python interpreter.")
+  ;;; ===============================
+  ;;  separable repl
+  ;;; ===============================
 
-  (defalias 'bc-guile-reconnect
-    #'jupyter-repl-restart-kernel
-    "Reconnect to the current REPL.")
+  (defvar-local bc-guile-repl-buffer nil
+    "The geiser repl associated to the current buffer.")
 
-  ;; when eval, send it to both the geiser REPL (for autocompletion)
-  ;; and jupyter REPL
+  (defun bc-guile--start-repl ()
+    "Start a geiser guile buffer."
+    (let ((code-buffer (current-buffer)))
+      (run-geiser geiser-default-implementation)
+      (let ((repl-buffer (current-buffer)))
+        (with-current-buffer code-buffer
+          (setq bc-guile-repl-buffer repl-buffer)))
+      (switch-to-buffer code-buffer)))
 
-  (defun bc-guile--eval-region (beg end)
-    "Evaluate region between BEG and END."
-    (geiser-eval-region beg end nil nil 'nomsg)
-    (jupyter-eval-region beg end))
+  (defun bc-guile--pop-to-repl ()
+    "Switch to `bc-guile-repl-buffer' associated with the current buffer."
+    (pop-to-buffer bc-guile-repl-buffer))
 
-  (defun bc-guile--eval-last-sexp ()
-    "Evaluate the last sexp before point."
-    ;; HACK: depend on evil
-    (save-excursion
-      (let* ((end (1+ (re-search-backward "[\])]")))
-             (beg (1- (evil-jump-item))))
-        (bc-guile--eval-region beg end))))
+  (defun bc-guile-start-or-pop-to-repl ()
+    "Pop to `bc-guile-repl-buffer'.  If `bc-guile-repl-buffer' is nil, start a new repl."
+    (if bc-guile-repl-buffer
+        (bc-guile--switch-to-repl)
+      (bc-guile--pop-to-repl)))
+
+  (defun bc-guile-associate-repl (repl-buffer)
+    "Select a geiser repl and associate the current buffer to."
+    (interactive
+     (list (ivy-read "Choose REPL to associate to"
+                     (seq-filter (lambda (bf)
+                                   (eq major-mode geiser-repl-mode)
+                                   (buffer-list)))
+                     :action 'identity)))
+    (setq bc-guile-repl-buffer repl-buffer))
 
   (defun bc-guile-eval-sexp-or-region ()
     (interactive)
     (if (region-active-p)
-        (bc-guile--eval-region (region-beginning) (region-end))
-      (bc-guile--eval-last-sexp)))
-
-  (defun bc-guile--repl-buffer-name (&rest _)
-    "Return the repl buffer name for the current buffer."
-    (format "*REPL-%s*" (buffer-name)))
-
-  (setq geiser-repl-buffer-name-function #'bc-guile--repl-buffer-name)
-
-  (defun bc-guile--advicing-geiser-repl (geiser-cmd &rest _)
-    "1. Open geiser REPL only if there is no one attached to the current buffer;
-     2. automatically delete window after REPL is called."
-    (unless (get-buffer (bc-guile--repl-buffer-name))
-      (funcall geiser-cmd)
-      (delete-window)))
-
-  (advice-add 'run-guile :around #'bc-guile--advicing-geiser-repl)
-
-  (defun bc-guile--init ()
-    "Init guile help and autocompletion."
-    (unless (string-match-p "jupyter-repl" (buffer-name))
-      (geiser-mode)
-      ;; HACK: the REPL fire up too quick I am still in the last buffer
-      (run-at-time 0.5 nil #'run-guile)))
-
-  (defun bc-guile--quit-repl ()
-    "Quit the corresponding geiser REPL associated with BUFFER-OR-NAME"
-    (when (and (eq major-mode 'scheme-mode)
-               (memq (get-buffer (bc-guile--repl-buffer-name)) (buffer-list)))
-      (cl-letf (((symbol-function 'y-or-n-p)
-                 (lambda (&rest _) t)))
-        (with-current-buffer (bc-guile--repl-buffer-name)
-          (kill-buffer-and-window)))))
-
-  (add-hook 'kill-buffer-hook #'bc-guile--quit-repl)
+        (geiser-eval-region (region-beginning) (region-end) nil nil 'nomsg)
+      (geiser-eval-last-sexp)))
   
 
   :general
@@ -88,10 +66,33 @@
    :prefix "SPC"
    "rr" 'bc-guile-eval-sexp-or-region
    "rh" 'geiser-doc-symbol-at-point
+   "rz" 'bc-guile-associate-repl
+   "ro" 'bc-guile-start-or-pop-to-repl)
 
-   "ro" 'bc-guile-local-repl
-   "rz" 'jupyter-repl-associate-buffer
-   "rZ" 'bc-guile-reconnect))
+  (:states '(motion normal visual insert)
+   :keymaps 'geiser-repl-mode-map
+   "<up>" 'comint-previous-matching-input-from-input
+   "<down>" 'comint-next-matching-input-from-input
+   "<return>" 'geiser-repl--maybe-send
+   "C-y" 'comint-previous-prompt
+   "C-e" 'comint-next-prompt)
+
+  (:keymaps 'geiser-repl-mode-map
+   :states '(normal visual motion insert)
+   :prefix "C-c"
+   "C-c" 'geiser-repl-interrupt)
+
+  (:keymaps 'geiser-repl-mode-map
+   :states '(normal visual motion)
+   :prefix "SPC"
+   "q" 'geiser-repl-exit)
+
+  (:keymaps 'geiser-repl-mode-map
+   :states '(normal visual motion)
+   "A" (lambda () (interactive)
+         (goto-char (point-max))
+         (evil-insert 1))
+   "SPC" nil))
 
 (provide 'bc-ide-guile)
 ;;; bc-ide-guile.el ends here
