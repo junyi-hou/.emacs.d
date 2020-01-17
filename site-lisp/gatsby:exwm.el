795518878741c2218d6f74ffb8b5a0d05e1abc03
Junyi <junyi.hou@berkeley.edu>
2020-01-17 09:55:38 -0800

    refactor exwm

--- site-lisp/gatsby:exwm.el
+++ site-lisp/gatsby:exwm.el
@@ -1,4 +1,4 @@
-;;; bc-exwm.el --- emacs X windowm manager config -*- lexical-binding: t; -*-
+;;; gatsby:exwm.el --- emacs X windowm manager config
 
 ;;; Commentary:
 
@@ -6,8 +6,7 @@
 
 (use-package exwm
   :init
-
-  (defun bc-exwm-amixer-adjust-volume (&optional up x)
+  (defun gatsby:exwm-amixer-adjust-volume (&optional up x)
     "Change volume by X percent, increase volume if UP is not-nil, otherwise decrease volume.  If X is nil, change by 5%."
     (let* ((x (or x 5))
            (direction (if up "+" "-")))
@@ -20,7 +19,7 @@
                                     (search-forward-regexp "\\(\\[\\([[:digit:]]+\\)%\\]\\)")
                                     (match-string 2))))))
 
-  (defun bc-exwm-amixer-toggle-mute ()
+  (defun gatsby:exwm-amixer-toggle-mute ()
     "Toggle mute."
     (interactive)
     (let* ((new-state (with-temp-buffer
@@ -30,7 +29,7 @@
                         (match-string 1))))
       (message "Master volume %s" new-state)))
 
-  (defun bc-exwm-adjust-backlight (&optional up)
+  (defun gatsby:exwm-adjust-backlight (&optional up)
     "Adjust backlight, if UP is non-nil, increase the brightness, otherwise decrease."
     (let* ((bl-file "/sys/class/backlight/intel_backlight/brightness")
            (bl-max "/sys/class/backlight/intel_backlight/max_brightness")
@@ -54,7 +53,7 @@
                             (float (string-to-number (buffer-string))))))
                  (* 100 (/ current max))))))
 
-  (defun bc-exwm-switch-to-workspace-confirm (index)
+  (defun gatsby:exwm-switch-to-workspace-confirm (index)
     "Switch to workspace INDEX, if it doesn't exists, ask whether to create it."
     (interactive
      (list
@@ -69,7 +68,7 @@
         (if (y-or-n-p (format "workspace %d doesn't exists, create?" index))
             (exwm-workspace-switch-create index)))))
 
-  (defun bc-exwm--get-xwindow-buffer ()
+  (defun gatsby:exwm--get-xwindow-buffer ()
     "Return a list of buffers containing currently opened Xwindows."
     (seq-filter
      (lambda (buffer)
@@ -77,61 +76,58 @@
          (equal major-mode 'exwm-mode)))
      (buffer-list)))
 
-  (defun bc-exwm-switch-to-xwindow ()
+  (defun gatsby:exwm-switch-to-xwindow ()
     "Choose a xwindow from `ivy-read' and display it in the current window or a new window in the current frame."
     (interactive)
     (ivy-read
      "swtich to: "
-     (mapcar 'buffer-name (bc-exwm--get-xwindow-buffer))
+     (mapcar 'buffer-name (gatsby:exwm--get-xwindow-buffer))
      :action
      (lambda (x)
        (when ivy-current-prefix-arg
-         (bc-core--split-window)
+         (gatsby:core--split-window)
          (other-window 1))
        (exwm-workspace-move-window
         (selected-frame)
         (exwm--buffer->id (get-buffer x))))))
 
   (when (executable-find "zathura")
-    (defun bc-exwm-open-pdf-in-zathura (ff &rest args)
+    (defun gatsby:exwm-open-pdf-in-zathura (ff &rest args)
       "If selected file is a pdf, open in `zathura'. Otherwise open using FF."
       (let ((filename (car args)))
         (if (string= "pdf" (file-name-extension filename))
             (progn
-              (bc-core-split-window)
+              (gatsby:core-split-window)
               (other-window 1)
               (start-process-shell-command "zathura"
                                            nil
                                            (format "zathura '%s'" filename)))
           (apply ff args))))
 
-    (advice-add #'find-file-noselect :around #'bc-exwm-open-pdf-in-zathura))
+    (advice-add #'find-file-noselect :around #'gatsby:exwm-open-pdf-in-zathura))
 
-  :config
-
-  ;;; ===============================
+  :config  
   ;;  multi-monitor setup
-  ;;; ===============================
 
   (require 'exwm-randr)
 
-  (defconst bc-exwm--default-monitor "eDP1"
+  (defconst gatsby:exwm--default-monitor "eDP1"
     "The internal screen output.")
 
-  (defvar bc-exwm--relative-layout nil
-    "How the position of external monitor relates to `bc-exwm--default-monitor'.")
+  (defvar gatsby:exwm--relative-layout nil
+    "How the position of external monitor relates to `gatsby:exwm--default-monitor'.")
 
-  (defvar bc-exwm--external-monitor-workspace-index nil
+  (defvar gatsby:exwm--external-monitor-workspace-index nil
     "The index of the workspace displayed on the external monitor.")
 
-  (defconst bc-exwm--direction-pairs-alist
+  (defconst gatsby:exwm--direction-pairs-alist
     '(("left" . "right")
       ("right" . "left")
       ("up" . "down")
       ("down" . "up"))
     "Pairs of direction, arranged as (dir . dir-opposite).")
 
-  (defun bc-exwm--monitor-status ()
+  (defun gatsby:exwm--monitor-status ()
     "Return lists of external monitor ports separately according to whether they are connected or not, i.e., (list of connected ports, list of disconnected ports)."
     (let* ((xrandr-output-regexp "\n\\([^ ]+\\) \\(dis\\)?connected ")
            (connected)
@@ -146,39 +142,39 @@
         ;; return
         `(,(seq-filter
             (lambda (mon)
-              (not (string= mon bc-exwm--default-monitor)))
+              (not (string= mon gatsby:exwm--default-monitor)))
             connected)
           ,disconnected))))
 
-  (defun bc-exwm--assign-workspaces (monitor)
-    "Assigning workspaces 1 - 8 to MONITOR and 0 to `bc-exwm--default-monitor'."
+  (defun gatsby:exwm--assign-workspaces (monitor)
+    "Assigning workspaces 1 - 8 to MONITOR and 0 to `gatsby:exwm--default-monitor'."
     (setq exwm-randr-workspace-monitor-plist
           (seq-reduce
            'append
            (mapcar (lambda (i) `(,i ,monitor)) (number-sequence 1 8))
-           `(0 ,bc-exwm--default-monitor))))
+           `(0 ,gatsby:exwm--default-monitor))))
 
-  (defun bc-exwm--turn-on-external-monitor (monitor position)
-    "Turn on external MONITOR.  POSITION determines the relative position of MONITOR to the builtin monitor `bc-exwm--default-monitor'."
-    (bc-exwm--assign-workspaces monitor)
+  (defun gatsby:exwm--turn-on-external-monitor (monitor position)
+    "Turn on external MONITOR.  POSITION determines the relative position of MONITOR to the builtin monitor `gatsby:exwm--default-monitor'."
+    (gatsby:exwm--assign-workspaces monitor)
     (call-process
      "xrandr" nil nil nil
      "--output" monitor position "eDP1" "--auto"))
 
-  (defun bc-exwm--tracking-external-monitor-workspace-index (index &optional index-range)
-    "Update `bc-exwm--external-monitor-workspace-index' to INDEX if INDEX is in INDEX-RANGE.
+  (defun gatsby:exwm--tracking-external-monitor-workspace-index (index &optional index-range)
+    "Update `gatsby:exwm--external-monitor-workspace-index' to INDEX if INDEX is in INDEX-RANGE.
 
 This function should be called after `exwm-workspace-switch' is called.
 
-HACK: only work in conjecture of `bc-exwm--assign-workspaces' and with 1 external monitors."
+HACK: only work in conjecture of `gatsby:exwm--assign-workspaces' and with 1 external monitors."
     (let ((index-range (or index-range (number-sequence 1 8))))
-      (when (and (car (bc-exwm--monitor-status))
+      (when (and (car (gatsby:exwm--monitor-status))
                  (member index index-range))
-        (setq bc-exwm--external-monitor-workspace-index index))))
+        (setq gatsby:exwm--external-monitor-workspace-index index))))
 
-  (advice-add 'exwm-workspace-switch :after 'bc-exwm--tracking-external-monitor-workspace-index)
+  (advice-add 'exwm-workspace-switch :after 'gatsby:exwm--tracking-external-monitor-workspace-index)
 
-  (defun bc-exwm--xrandr-to-direction (xrandr-argument)
+  (defun gatsby:exwm--xrandr-to-direction (xrandr-argument)
     "Translate XRANDR-ARGUMENT to direction"
     (string-match "\\(right\\|left\\|above\\|below\\)" xrandr-argument)
     (let ((result (match-string 1 xrandr-argument)))
@@ -187,13 +183,13 @@ HACK: only work in conjecture of `bc-exwm--assign-workspaces' and with 1 externa
        ((string= result "below") "down")
        (t result))))
 
-  (defun bc-exwm--windmove-most (dir)
+  (defun gatsby:exwm--windmove-most (dir)
     "Move to the DIR most window of the `selected-frame'."
     (ignore-errors
       (while t
         (windmove-do-window-select (obarray-get obarray dir)))))
 
-  (defun bc-exwm--adviced-p (predicates fun)
+  (defun gatsby:exwm--adviced-p (predicates fun)
     "Return FUN's advices that satisfies PREDICATES.  If there is no advice that satisfies PREDICATES or there is no advice at all, return nil.
 
 Adapted from https://emacs.stackexchange.com/questions/33020/how-can-i-remove-an-unnamed-advice/33021#33021."
@@ -205,23 +201,23 @@ Adapted from https://emacs.stackexchange.com/questions/33020/how-can-i-remove-an
        fun)
       (nreverse result)))
 
-  (defun bc-exwm--windmove-advice-remove ()
+  (defun gatsby:exwm--windmove-advice-remove ()
     "Remove advice of windmove-{left,right,up,down} so it is what it was."
     (dolist (fun '(windmove-left windmove-right windmove-up windmove-down))
-      (let ((advice (bc-exwm--adviced-p
-                     (lambda (f) (string-match-p "bc-exwm--" (symbol-name f)))
+      (let ((advice (gatsby:exwm--adviced-p
+                     (lambda (f) (string-match-p "gatsby:exwm--" (symbol-name f)))
                      fun)))
         (when advice
           (advice-remove fun (car advice))))))
 
-  (defun bc-exwm--windmove-advice-add (position)
+  (defun gatsby:exwm--windmove-advice-add (position)
     "Generate functions based on external monitor's relative POSITION and advicing them to windmove-{left, right, up, down}."
-    (let* ((dir (bc-exwm--xrandr-to-direction position))
-           (opp-dir (cdr (assoc dir bc-exwm--direction-pairs-alist)))
+    (let* ((dir (gatsby:exwm--xrandr-to-direction position))
+           (opp-dir (cdr (assoc dir gatsby:exwm--direction-pairs-alist)))
            (windmove-same (obarray-get obarray (concat "windmove-" dir)))
            (windmove-oppo (obarray-get obarray (concat "windmove-" opp-dir))))
 
-      (defun bc-exwm--advice-windmove-same (windmove &rest args)
+      (defun gatsby:exwm--advice-windmove-same (windmove &rest args)
         (if (eq exwm-workspace-current-index 0)
             ;; in internal monitor
             (condition-case nil
@@ -231,12 +227,12 @@ Adapted from https://emacs.stackexchange.com/questions/33020/how-can-i-remove-an
               ;; need to move to the opposite-most window of the internal monitor
               (error
                (progn
-                 (exwm-workspace-switch bc-exwm--external-monitor-workspace-index)
-                 (bc-exwm--windmove-most opp-dir))))
+                 (exwm-workspace-switch gatsby:exwm--external-monitor-workspace-index)
+                 (gatsby:exwm--windmove-most opp-dir))))
           ;; in external monitor, not affected
           (apply windmove args)))
 
-      (defun bc-exwm--advice-windmove-oppo (windmove &rest args)
+      (defun gatsby:exwm--advice-windmove-oppo (windmove &rest args)
         (if (eq exwm-workspace-current-index 0)
             ;; in internal monitor, not affected
             (apply windmove args)
@@ -249,17 +245,17 @@ Adapted from https://emacs.stackexchange.com/questions/33020/how-can-i-remove-an
             (error
              (progn
                (exwm-workspace-switch 0)
-               (bc-exwm--windmove-most dir))))))
+               (gatsby:exwm--windmove-most dir))))))
 
-      (advice-add windmove-same :around #'bc-exwm--advice-windmove-same)
-      (advice-add windmove-oppo :around #'bc-exwm--advice-windmove-oppo)))
+      (advice-add windmove-same :around #'gatsby:exwm--advice-windmove-same)
+      (advice-add windmove-oppo :around #'gatsby:exwm--advice-windmove-oppo)))
 
   ;; automatically adjust display when external monitor plug in/out
-  (defun bc-exwm--auto-adjust-display ()
+  (defun gatsby:exwm--auto-adjust-display ()
     "Automatically adjust display.
 
-This function first scan for video port status via `bc-exwm--monitor-status', then use xrandr to turn on/off screens, finally, according to the relative position of the internal/external monitor, advice windmove functions."
-    (let* ((port-status (bc-exwm--monitor-status))
+This function first scan for video port status via `gatsby:exwm--monitor-status', then use xrandr to turn on/off screens, finally, according to the relative position of the internal/external monitor, advice windmove functions."
+    (let* ((port-status (gatsby:exwm--monitor-status))
            (connected (caar port-status))
            (disconnected (cadr port-status)))
 
@@ -269,29 +265,27 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
                "--output" monitor "--off"))
             disconnected)
 
-      (bc-exwm--windmove-advice-remove)
+      (gatsby:exwm--windmove-advice-remove)
 
       (when connected
         (let ((position (ivy-read
                          "Relative position: "
                          '("--right-of" "--left-of" "--above" "--below" "--same-as")
                          :action 'identity)))
-          (setq bc-exwm--relative-layout position)
-          (bc-exwm--turn-on-external-monitor connected position)
-          (bc-exwm--windmove-advice-add position)
+          (setq gatsby:exwm--relative-layout position)
+          (gatsby:exwm--turn-on-external-monitor connected position)
+          (gatsby:exwm--windmove-advice-add position)
 
           ;; finally, init the default workspace on the external monitor
-          (setq bc-exwm--external-monitor-workspace-index 1)))))
+          (setq gatsby:exwm--external-monitor-workspace-index 1)))))
 
-  (add-hook 'exwm-randr-screen-change-hook #'bc-exwm--auto-adjust-display)
-
-  ;;; ===============================
+  (add-hook 'exwm-randr-screen-change-hook #'gatsby:exwm--auto-adjust-display)
+  
   ;;  org-integration
-  ;;; ===============================
 
   (with-eval-after-load 'org
     (progn
-      (defmacro bc-exwm--capture (app type link description &rest args)
+      (defmacro gatsby:exwm--capture (app type link description &rest args)
         "Capture LINK of TYPE with DESCRIPTION in exwm APP."
         `(lambda ()
            (when (and (equal major-mode 'exwm-mode)
@@ -303,22 +297,20 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
 
       (org-link-set-parameters
        "zathura"
-       :store (bc-exwm--capture "Zathura" "file" (buffer-name) ""))
+       :store (gatsby:exwm--capture "Zathura" "file" (buffer-name) ""))
 
       ;; HACK - need to tell qutebrowser to set window-title = url
       (org-link-set-parameters
        "qutebrowser"
-       :store (bc-exwm--capture "qutebrowser" "http" (buffer-name) ""))))
-
-  ;;; ===============================
+       :store (gatsby:exwm--capture "qutebrowser" "http" (buffer-name) ""))))
+  
   ;;  Settings
-  ;;; ===============================
 
   ;; rename buffer to window title
-  (defun bc-exwm-rename-buffer-to-title ()
+  (defun gatsby:exwm-rename-buffer-to-title ()
     (exwm-workspace-rename-buffer exwm-title))
 
-  (add-hook 'exwm-update-title-hook 'bc-exwm-rename-buffer-to-title)
+  (add-hook 'exwm-update-title-hook 'gatsby:exwm-rename-buffer-to-title)
   (add-hook 'exwm-floating-setup-hook 'exwm-layout-hide-mode-line)
   (add-hook 'exwm-floating-exit-hook 'exwm-layout-show-mode-line)
 
@@ -329,7 +321,7 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
   (evil-set-initial-state 'exwm-mode 'emacs)
 
   ;; make sure the exwm buffer is always in emacs state
-  (defun bc-exwm--back-emacs-state ()
+  (defun gatsby:exwm--back-emacs-state ()
     "Automatically switch back to `evil-emacs-state' if not already in it."
     (when (and (equal major-mode 'exwm-mode)
                (not (evil-emacs-state-p)))
@@ -337,17 +329,17 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
 
   (add-hook
    'exwm-manage-finish-hook
-   (defun bc-exwm--hook ()
-     (add-hook 'post-command-hook #'bc-exwm--back-emacs-state nil t)
+   (defun gatsby:exwm--hook ()
+     (add-hook 'post-command-hook #'gatsby:exwm--back-emacs-state nil t)
 
-     (defun bc-exwm--delete-window-when-kill ()
+     (defun gatsby:exwm--delete-window-when-kill ()
        (when (< (length (window-list)) 2)
          (switch-to-buffer-other-window "*scratch*")
          (other-window -1))
        (delete-window))
 
-     (add-hook 'kill-buffer-hook #'bc-exwm--delete-window-when-kill nil t)))
-
+     (add-hook 'kill-buffer-hook #'gatsby:exwm--delete-window-when-kill nil t)))
+  
   ;; keys
 
   ;; keys that should be interpreted by emacs in
@@ -366,20 +358,20 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
                       `(,(kbd (format "s-%d" i)) .
                         (lambda ()
                           (interactive)
-                          (bc-exwm-switch-to-workspace-confirm (- ,i 1)))))
+                          (gatsby:exwm-switch-to-workspace-confirm (- ,i 1)))))
                     (number-sequence 1 9))
           ;; the last workspace (9) is s-h
-          ([?\s-h] . (lambda () (interactive) (bc-exwm-switch-to-workspace-confirm 9)))
+          ([?\s-h] . (lambda () (interactive) (gatsby:exwm-switch-to-workspace-confirm 9)))
           ;; multimedia keys
           ([XF86AudioLowerVolume] . (lambda () (interactive)
-                                      (bc-exwm-amixer-adjust-volume)))
-          ([XF86AudioMute] . bc-exwm-amixer-toggle-mute)
+                                      (gatsby:exwm-amixer-adjust-volume)))
+          ([XF86AudioMute] . gatsby:exwm-amixer-toggle-mute)
           ([XF86AudioRaiseVolume] . (lambda () (interactive)
-                                      (bc-exwm-amixer-adjust-volume t)))
+                                      (gatsby:exwm-amixer-adjust-volume t)))
           ([XF86MonBrightnessUp] . (lambda () (interactive)
-                                     (bc-exwm-adjust-backlight t)))
+                                     (gatsby:exwm-adjust-backlight t)))
           ([XF86MonBrightnessDown] . (lambda () (interactive)
-                                       (bc-exwm-adjust-backlight)))))
+                                       (gatsby:exwm-adjust-backlight)))))
 
   ;; simulation key
   (setq exwm-input-simulation-keys
@@ -432,21 +424,25 @@ This function first scan for video port status via `bc-exwm--monitor-status', th
                                                   nil
                                                   (format "zathura '%s'" x)))))))
 
+;; TODO: improve this
 (use-package exwm-edit
   ;; C-c ' in X windows
   :straight
-  (exwm-edit
-   :repo "junyi-hou/exwm-edit"
-   :host github)
+  (exwm-edit :repo "junyi-hou/exwm-edit" :host github)
   :init
   (add-hook 'exwm-edit-compose-hook #'evil-insert-state)
 
-  (defun bc-exwm--auto-select-all (&rest args)
+  (defun gatsby:exwm--auto-select-all (&rest args)
     "Advice `exwm-edit--compose' to send fake <C-a> to the exwm window."
     (exwm-input--fake-key ?\C-a)
     (sleep-for 0.05))
 
-  (advice-add 'exwm-edit--compose :before #'bc-exwm--auto-select-all))
+  (advice-add 'exwm-edit--compose :before #'gatsby:exwm--auto-select-all))
 
-(provide 'bc-exwm)
-;;; bc-exwm.el ends here
+(provide 'gatsby:exwm)
+
+;; Local Variables:
+;; lexical-binding: t
+;; End:
+
+;;; gatsby:exwm.el ends here
