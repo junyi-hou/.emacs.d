c05363b421afa3251cba8e59de27c52731db5a0a
Junyi <junyi.hou@berkeley.edu>
2020-01-16 18:39:33 -0800

    fix beacon bug

--- site-lisp/bc-theme.el
+++ site-lisp/bc-theme.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+(require 'bc-core)
+
 ;; theme
 (use-package gruvbox-theme
   :init
@@ -148,7 +150,7 @@
 ;; visual cue of the cursor position
 (use-package beacon
   ;; use chep's fork so it can grow backwards
-  :straight (beacon :host github :repo "chep/beacon")
+  :straight (beacon :host github :repo "junyi/beacon")
   :config
   (setq beacon-blink-when-window-scrolls nil
         beacon-can-go-backwards t
