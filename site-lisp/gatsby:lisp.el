b9e97a1857812b883909a93973b1e35b2d0f45a6
Junyi <junyi.hou@berkeley.edu>
2020-01-17 11:10:00 -0800

    refactor lisp

--- site-lisp/gatsby:lisp.el
+++ site-lisp/gatsby:lisp.el
@@ -1,54 +1,56 @@
-;;; bc-lisp.el --- setup ide features (auto-completion, code-jump, linter) for lisp -*- lexical-binding: t; -*-
+;;; gatsby:lisp.el --- setup ide features (auto-completion, code-jump, linter) for lisp -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
 ;;; Code:
+(require 'gatsby:core)
+(require 'gatsby:comint)
 
 (use-package elisp-mode
   :straight (:type built-in)
   :init
   ;; fix tab width
-  (defun bc-lisp--set-tab-width ()
+  (defun gatsby:lisp--set-tab-width ()
     (setq-local tab-width 2))
 
   :hook
-  (emacs-lisp-mode . bc-lisp--set-tab-width))
+  (emacs-lisp-mode . gatsby:lisp--set-tab-width))
 
 (use-package ielm
   :straight (:type built-in)
   :hook (ielm-mode . company-mode)
-  :commands (bc-lisp-start-or-pop-to-repl)
+  :commands (gatsby:lisp-start-or-pop-to-repl)
   :init
 
-  (defun bc-lisp-start-or-pop-to-repl ()
+  (defun gatsby:lisp-start-or-pop-to-repl ()
     "Pop to the associated REPL, if such REPL does not exist, start one."
     (interactive)
-    (if bc-comint-repl-buffer
-        (bc-comint--pop-to-repl)
-      (bc-core-split-window)
+    (if gatsby:comint-repl-buffer
+        (gatsby:comint--pop-to-repl)
+      (gatsby:core-split-window)
       (other-window 1)
-      (bc-comint--start-repl 'ielm)))
+      (gatsby:comint--start-repl 'ielm)))
 
-  (defun bc-lisp-associate-repl ()
+  (defun gatsby:lisp-associate-repl ()
     "Associate current buffer to a REPL"
     (interactive)
-    (bc-comint-associate-repl 'inferior-emacs-lisp-mode))
+    (gatsby:comint-associate-repl 'inferior-emacs-lisp-mode))
 
-  (defun bc-lisp-eval-sexp-or-region ()
+  (defun gatsby:lisp-eval-sexp-or-region ()
     (interactive)
     (if (region-active-p)
-        (bc-comint--eval-region 'ielm-return (region-beginning) (region-end))
-      (bc-comint--eval-last-sexp 'ielm-return)))
+        (gatsby:comint--eval-region 'ielm-return (region-beginning) (region-end))
+      (gatsby:comint--eval-last-sexp 'ielm-return)))
 
   :general
   (:keymaps '(emacs-lisp-mode-map
               lisp-interaction-mode-map)
    :states '(motion normal visual)
    :prefix "SPC"
-   "rr" 'bc-lisp-eval-sexp-or-region
+   "rr" 'gatsby:lisp-eval-sexp-or-region
    "rh" 'helpful-symbol
-   "rz" 'bc-lisp-associate-repl
-   "ro" 'bc-lisp-start-or-pop-to-repl)
+   "rz" 'gatsby:lisp-associate-repl
+   "ro" 'gatsby:lisp-start-or-pop-to-repl)
 
   (:keymaps 'inferior-emacs-lisp-mode-map
    :states '(normal motion visual)
@@ -61,7 +63,7 @@
   (:keymaps 'inferior-emacs-lisp-mode-map
    :states '(normal motion visual)
    :prefix "SPC"
-   "q" 'bc-comint-exit-repl))
+   "q" 'gatsby:comint-exit-repl))
 
 (use-package aggressive-indent
   :hook
@@ -74,5 +76,5 @@
 (use-package easy-escape
   :hook ((emacs-lisp-mode lisp-mode ielm-mode) . easy-escape-minor-mode))
 
-(provide 'bc-lisp)
-;;; bc-lisp.el ends here
+(provide 'gatsby:lisp)
+;;; gatsby:lisp.el ends here
