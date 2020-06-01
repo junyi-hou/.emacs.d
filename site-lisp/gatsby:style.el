349c8f2a3f6eb5778635c048a6a2d93fab8ce085
Junyi <junyi.hou@berkeley.edu>
2020-05-31 22:56:47 -0700

    integrate selectrum with flyspell-correct

--- site-lisp/gatsby:style.el
+++ site-lisp/gatsby:style.el
@@ -6,12 +6,39 @@
 
 (require 'gatsby:core)
 
-;; TODO: better SPC-sp
-;; TODO: integrate with selectrum
-;; (use-package flyspell-correct
+(use-package flyspell-correct
   :hook
   (text-mode . flyspell-mode)
   (prog-mode . flyspell-prog-mode)
+
+  :init
+  (defun gatsby:style-flyspell-correct-selectrum (candidates word)
+    "Run `completing-read' for the given CANDIDATES
+
+List of CANDIDATES is given by flyspell for the WORD."
+    (let* ((selectrum-should-sort-p nil)
+           (save "[SAVE]")
+           (accept-session "[ACCEPT (session)]")
+           (accept-buffer "[ACCEPT (buffer)]")
+           (skip "[SKIP]")
+           (result (completing-read
+                    (format "Correcting '%s': " word)
+                    (append (list save accept-session accept-buffer skip)
+                            candidates)
+                    nil nil nil nil (car candidates))))
+      (cond
+       ((string= result save)
+        (cons 'save word))
+       ((string= result accept-session)
+        (cons 'session word))
+       ((string= result accept-buffer)
+        (cons 'buffer word))
+       ((string= result skip)
+        (cons 'skip word))
+       (t
+        result))))
+
+  (setq flyspell-correct-interface #'gatsby:style-flyspell-correct-selectrum)
 
   :general
   (:keymaps '(motion normal visual)
@@ -19,7 +46,7 @@
    "sp" 'gatsby:style-correct)
   (:keymaps 'insert
    :prefix "C-c"
-   "s" 'gatsby:style-correct))
+   "s" 'flyspell-correct-wrapper))
 
 (provide 'gatsby:style)
 ;;; gatsby:style.el ends here
