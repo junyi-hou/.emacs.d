6d99a101b88515984b50ebc3bd2da85a2ccf8ab5
Junyi <junyi.hou@berkeley.edu>
2020-07-10 19:51:39 -0700

    update mode-line config

--- site-lisp/gatsby:theme.el
+++ site-lisp/gatsby:theme.el
@@ -74,7 +74,103 @@
 
   :config
   ;; custom mode-line
-  ;; TODO
+
+  (defvar mode-line-current-window (frame-selected-window))
+
+  (defun mode-line-set-current-window (&rest _)
+    (when (and (not (minibuffer-window-active-p (frame-selected-window))) ;1
+               (not (frame-parameter (selected-frame) 'parent-frame)) ; 2
+               ;; 1: not minibufer window
+               ;; 2: not child frame
+               )
+      (setq mode-line-current-window (frame-selected-window))
+      (force-mode-line-update)))
+
+  (defun mode-line-unset-current-window ()
+    (setq mode-line-current-window nil)
+    (force-mode-line-update))
+
+  (defun mode-line-current-window-active-p ()
+    (eq mode-line-current-window (selected-window)))
+
+  (add-hook 'window-configuration-change-hook #'mode-line-set-current-window)
+  (add-hook 'focus-in-hook #'mode-line-set-current-window)
+  (add-hook 'focus-out-hook #'mode-line-unset-current-window)
+  (advice-add 'handle-switch-frame :after #'mode-line-set-current-window)
+
+  ;; allow mode line to be inactive when lose focus
+  (copy-face 'mode-line 'mode-line-active-face)
+  (defun mode-line-focus-out ()
+    (copy-face 'mode-line-inactive 'mode-line))
+  (defun mode-line-focus-in ()
+    (copy-face 'mode-line-active-face 'mode-line))
+
+  (add-hook 'focus-in-hook #'mode-line-focus-in)
+  (add-hook 'focus-out-hook #'mode-line-focus-out)
+  ;; segments
+  (defun mode-line-evil-face ()
+    "Return the appropriate color foreground for `evil-mode-line-tag'"
+    `(:weight bold
+      :foreground
+      ,(cond ((not (mode-line-current-window-active-p))
+              (face-attribute 'mode-line-inactive :foreground))
+             ((or (evil-normal-state-p) (evil-motion-state-p)) "#98C379")
+             ((or (evil-insert-state-p)
+                  (evil-replace-state-p)
+                  (evil-operator-state-p)) "#d16969")
+             ((evil-visual-state-p) "#E5C07B")
+             ((evil-emacs-state-p) "#C586C0"))))
+
+  (defun mode-line-git-info ()
+    "Return the repo name and current branch."
+    (if-let ((vc-p (cdr (project-current))))
+        (let* ((repo (file-name-nondirectory (directory-file-name vc-p)))
+               (branch (substring-no-properties vc-mode 5)))
+          (propertize (concat "[" repo "::" branch "]") 'face
+                      `(:foreground
+                        ,(face-attribute (if (mode-line-current-window-active-p)
+                                             'mode-line
+                                           'mode-line-inactive)
+                                         :foreground))))
+      ""))
+
+  (defun mode-line-buf-name ()
+    "Patch mode-line-buf-name to have different text property."
+    (propertize (buffer-name)
+                'face `(:foreground
+                        ,(cond
+                          ((not (mode-line-current-window-active-p))  ;; not focused
+                           (face-attribute 'mode-line-inactive :foreground))
+                          ((or (buffer-modified-p)
+                               (not (buffer-file-name)))  ;; modified or non-file
+                           (face-attribute 'font-lock-warning-face :foreground))
+                          (buffer-read-only
+                           (face-attribute 'minibuffer-prompt :foreground))
+                          (t (face-attribute 'mode-line :foreground))))))
+
+  (defun mode-line-remote-p ()
+    (propertize
+     (if (file-remote-p default-directory) "[R]" "")
+     'face (if (mode-line-current-window-active-p) 'mode-line 'mode-line-inactive)))
+
+  (setq-default mode-line-format
+                (list
+                 '(:eval (propertize evil-mode-line-tag
+                                     'face (mode-line-evil-face)))
+                 '(:eval (mode-line-git-info))
+                 " "
+                 '(:eval (mode-line-remote-p))
+                 '(:eval (mode-line-buf-name))
+                 ;; align to the right
+                 '(:eval (propertize " " 'display
+                                     `((space :align-to (- (+ right
+                                                              right-fringe
+                                                              right-margin-width)
+                                                           ,(+ 3 (string-width
+                                                                  mode-name)))))))
+                 ;; major mode
+                 " %m"))
+
 
   :general
   (:keymaps '(motion normal visual emacs insert)
@@ -90,6 +186,11 @@
   :custom
   (highlight-indent-guides-method 'bitmap)
   (highlight-indent-guides-responsive 'stack))
+
+;; highlight color codes
+(use-package rainbow-mode
+  :defer t
+  :hook (prog-mode . rainbow-mode))
 
 ;; visual cue of the cursor position
 (use-package beacon
