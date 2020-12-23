5b9cffabb555c1adc9d662695063693f7b03fe50
Junyi <junyi.hou@berkeley.edu>
2020-12-23 11:14:40 -0800

    fix bug in selectrum-unify-tab

--- site-lisp/gatsby:minibuffer.el
+++ site-lisp/gatsby:minibuffer.el
@@ -15,42 +15,6 @@
   :init
   (selectrum-mode 1)
 
-  (defun gatsby:selectrum-jump-to-hs-header ()
-    "Provide a list of `hideshow' headers from which one can jump to."
-    (interactive)
-    (unless hs-block-start-regexp
-      (user-error "Cannot find `hs-block-start-regexp', enable `hs-minor-mode' first"))
-    (let* ((selectrum-should-sort-p nil)
-           (headings
-            (cl-loop with buffer-text-lines = (split-string (buffer-string) "\n")
-                     for txt in buffer-text-lines
-                     for num from 1 to (1- (length buffer-text-lines))
-                     ;; HACK:
-                     ;; only include `hideshow' headers, identified using
-                     ;; `hs-block-start-regexp'
-                     ;; also exclude any line in comments or in docs or in string
-                     when (and (string-match hs-block-start-regexp txt)
-                               (not (seq-some (lambda (p)
-                                                (or (eq p font-lock-doc-face)
-                                                    (eq p font-lock-comment-face)
-                                                    (eq p font-lock-string-face)))
-                                              (text-properties-at (- (length txt) 2) txt))))
-                     collect (propertize (concat (number-to-string num)
-                                                 " "
-                                                 txt
-                                                 " ...")
-                                         'line-num num)))
-           (chosen-heading (completing-read "Jump to: "
-                                            headings nil t nil
-                                            'gatsby:selectrum-jump-history))
-           (jump-position (get-text-property 0 'line-num chosen-heading)))
-      (goto-char (point-min))
-      (forward-line jump-position)
-      (when (memq 'hs-minor-mode minor-mode-list)
-        (save-excursion
-          (hs-show-block)))
-      (call-interactively #'recenter)))
-
   (defun gatsby:selectrum--remove-until-slash (bound n)
     "Return the position of the backwards Nth slash until BOUND.
 If no slash was found, return BOUND."
@@ -94,52 +58,39 @@ If no slash was found, return BOUND."
                 (1- (length selectrum--refined-candidates))
               (1- selectrum--current-candidate-index)))))
 
-  (defun gatsby:selectrum--combine (input common)
-    "Combine INPUT with COMMON that are prescient regexp conscious.
-Example: input = \"/home/gat sel\"
-        common = \"gatsby:selectrum.el\"
-        output = \"/home/gatsby:selectrum.el\"
-This function takes advantage of `selectrum-prescient--highlight' which highlights the matching part of COMMON in INPUT. To combine those two, I can \"fill the blanks\" in INPUT by getting un-highlighted parts of COMMON"
-    (let* ((input "/home/gat sel")
-           (common (buffer-name))
-           ;; COMMON does not have highlight information, so get it from the first
-           ;; candidate
-           (common (s-shared-start
-                    (buffer-substring (line-beginning-position 2)
-                                      (line-end-position 2))
-                    common)))
-      (let (out
-            (in (split-string input " " 'omit-nulls)))
-        in))
-    ;; now fill in the blanks
-    )
-
   (defun gatsby:selectrum-unified-tab ()
-    "TODO: incorporate `gatsby:selectrum--combine'."
+    "<tab> does the following things
+1. if there is a common part among candidates, complete the common part;
+2. if there is only one candidate, select the candidate
+3. if the last command is `gatsby:selectrum-unified-tab', `gatsby:selectrum-previous-candidate-cycle' or `gatsby:selecturm-next-candidate-cycle', then select the current candidate"
     (interactive)
     (when selectrum--current-candidate-index
-      (let* ((candidate (selectrum--get-full (nth selectrum--current-candidate-index
-                                                  selectrum--refined-candidates)))
-             (input (buffer-substring-no-properties
+      (let* ((input (buffer-substring-no-properties
                      selectrum--start-of-input-marker
-                     selectrum--end-of-input-marker)))
-        (if (string= input candidate)
-            (selectrum-select-current-candidate)
-          (when-let* ((common (try-completion "" selectrum--refined-candidates))
-                      (common (or (and (not (string= "" common)) common)
-                                  (and (eq this-command last-command) candidate))))
+                     selectrum--end-of-input-marker))
+             (common (try-completion "" selectrum--refined-candidates)))
+        (cond
+         ;; case 2/3
+         ((or (memq last-command '(gatsby:selectrum-unified-tab
+                                   gatsby:selectrum-next-candidate-cycle
+                                   gatsby:selectrum-previous-candidate-cycle))
+              (= 1 (length selectrum--refined-candidates)))
+          (selectrum-select-current-candidate))
+         ;; case 1
+         ((not (string= common ""))
+          (progn
             (delete-region (gatsby:selectrum--remove-until-slash
                             selectrum--start-of-input-marker 1)
-                           (point))
-            (insert common))))))
-  
+                           selectrum--end-of-input-marker)
+            (insert common)))))))
+
   :config
   (setq selectrum-minibuffer-bindings
         (append selectrum-minibuffer-bindings
                 '(("M-j" . gatsby:selectrum-next-candidate-cycle)
                   ("M-k" . gatsby:selectrum-previous-candidate-cycle)
                   ("<backspace>" . gatsby:selectrum-better-backspace)
-                  ("TAB" . gatsby:selectrum-unified-tab))))
+                  ("<tab>" . gatsby:selectrum-unified-tab))))
 
   :custom
   (selectrum-fix-minibuffer-height t)
@@ -156,8 +107,7 @@ This function takes advantage of `selectrum-prescient--highlight' which highligh
                                         nil t))))
    "ob" 'switch-to-buffer
    "om" (lambda () (interactive)
-          (switch-to-buffer-other-window (get-buffer-create "*Messages*")))
-   "oj" 'gatsby:selectrum-jump-to-hs-header))
+          (switch-to-buffer-other-window (get-buffer-create "*Messages*")))))
 
 (use-package selectrum-prescient
   :after selectrum
